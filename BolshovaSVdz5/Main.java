a33b03006a0da0dfdaee64fb92f189669c3e4069
SvetlanaBolshova <80790487+SvetlanaBolshova@users.noreply.github.com>
2021-08-02 13:22:44 +0300

    Add files via upload

---
+++ BolshovaSVdz5/Main.java
@@ -0,0 +1,15 @@
+package BolshovaSVdz5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AppData appData = new AppData();
+        appData.load("dz.txt");
+
+        System.out.println(Arrays.toString(appData.getHeader()));
+        System.out.println(Arrays.deepToString(appData.getData()));
+
+    }
+}
