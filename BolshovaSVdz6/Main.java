e4aef94e0c1bb25ad71c5a3f60fc5b952d4d698d
SvetlanaBolshova <80790487+SvetlanaBolshova@users.noreply.github.com>
2021-08-08 22:13:51 +0300

    Add files via upload

---
+++ BolshovaSVdz6/Main.java
@@ -0,0 +1,11 @@
+package BolshovaSVdz6;
+
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
+
